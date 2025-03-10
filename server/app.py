960f1f13c592e846870fc7ca78650365cf9ee123
Liam Brophy <liambrophy.design@gmail.com>
2025-03-10 16:54:04 -0400

    completes lab

--- server/app.py
+++ server/app.py
@@ -1,21 +1,21 @@
 from flask import Flask
-from flask_migrate import Migrate
-
-from models import db
+from server.models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-migrate = Migrate(app, db)
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
+# Import your models to register them with SQLAlchemy
+from server.models import Customer, Item, Review
 
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
 
-
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
